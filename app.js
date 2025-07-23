3031032e8a5440fba256955bdc83fd87b5f41f67
HigherGround189 <thehigherground18@gmail.com>
2025-07-23 23:15:48 +0800

    v-for

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const app = Vue.createApp({
   data() {
     return {
       showBooks: true,
-      title: "The Final Empire",
-      author: "Brandon Sanderson",
-      age: 45,
-      x: 0,
-      y: 0,
+      books: [
+        {title: "Name of the Wind", author: "Patrick Rothfuss"},
+        {title: "Way of Kings", author: "Brandon Sanderson"},
+        {title: "The Final Empire", author: "Brandon Sanderson"},
+      ]
     }
   },
   methods: {
