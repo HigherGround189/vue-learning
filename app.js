1316611c6378e93ed0c312b2f9d2179b2f500ccc
HigherGround189 <thehigherground18@gmail.com>
2025-07-23 23:03:45 +0800

    mouse events

--- app.js
+++ app.js
@@ -4,12 +4,21 @@ const app = Vue.createApp({
       showBooks: true,
       title: "The Final Empire",
       author: "Brandon Sanderson",
-      age: 45
+      age: 45,
+      x: 0,
+      y: 0,
     }
   },
   methods: {
     toggleShowBooks() {
       this.showBooks = !this.showBooks
+    },
+    handleEvent(event, second) {
+      console.log(event.type, second)
+    },
+    handleMousemove(event) {
+      this.x = event.offsetX
+      this.y = event.offsetY
     }
   }
 })
