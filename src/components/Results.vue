979ee8ea005faeb341f0e41685b89d6dc192b3fa
HigherGround189 <thehigherground18@gmail.com>
2025-08-02 20:16:43 +0800

    finishing touches

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,15 +1,38 @@
 <template>
-  <div>
-    <p v-if="showResults">Reaction Time: {{ score }} </p>
+  <div v-if="showResults">
+    <p>Reaction Time: {{ score }} </p>
+    <p class="rank">{{ rank }}</p>
   </div>
 </template>
 
 <script>
   export default {
-    props: ['showResults' ,'score']
+    props: ['showResults' ,'score'],
+    data() {
+      return {
+        rank: null
+      }
+    },
+    mounted() {
+      if (this.score < 250) {
+        this.rank = "Ninja Fingers"
+      }
+
+      else if (this.score < 400) {
+        this.rank = "Rapid Reflexes"
+      }
+
+      else {
+        this.rank = "Snail's pace..."
+      }
+    }
   }
 </script>
 
 <style scoped>
-
+  .rank {
+    font-size: 1.4rem;
+    color: #0faf87;
+    font-weight: bold;
+  }
 </style>
