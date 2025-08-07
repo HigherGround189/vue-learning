3050a1629f032f84a0c5ee8f2e238fd5552bfb57
HigherGround189 <thehigherground18@gmail.com>
2025-08-07 23:43:32 +0800

    finished setup

---
+++ src/components/Circles.vue
@@ -0,0 +1,22 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style scoped>
+  div {
+    border-radius: 50%;
+    background-color: hsl(153, 48%, 49%);
+    aspect-ratio: 1 / 1;
+    width: 5vh;
+  }
+
+  div:hover {
+    width: 10vh;
+  }
+</style>
